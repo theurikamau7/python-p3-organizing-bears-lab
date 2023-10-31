e6653d896ac489fbc60ef982dab7fb2c03e7e18e
IanKamau <theurikamau7@gmail.com>
2023-10-31 09:41:06 +0300

    completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES
+    ('Bear 1', 5, 'Male', 'Brown', 'Calm', 1),
+    ('Bear 2', 3, 'Female', 'Black', 'Aggressive', 1),
+    ('Bear 3', 7, 'Male', 'Blonde', 'Playful', 1),
+    ('Bear 4', 4, 'Female', 'Brown', 'Calm', 1),
+    ('Bear 5', 6, 'Male', 'Black', 'Aggressive', 1),
+    ('Bear 6', 2, 'Female', 'Blonde', 'Playful', 1),
+    (NULL, 8, 'Male', 'Brown', 'Calm', 1),
+    ('Bear 8', 1, 'Female', 'Black', 'Aggressive', 1);
+
+
+-- INSERT INTO bears (name, age, sex, color, temperament, alive)
+-- VALUES (NULL, 10, 'Male', 'Brown', 'Calm', 1);
--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -1,18 +1,43 @@
+import sqlite3
+
+connection = sqlite3.connect(":memory:")
+cursor = connection.cursor()
+
+create_file = open("lib/create.sql")
+create_as_string = create_file.read()
+cursor.executescript(create_as_string)
+
+insert_file = open("lib/seed.sql")
+insert_as_string = insert_file.read()
+cursor.executescript(insert_as_string)
+
 select_all_female_bears_return_name_and_age = """
-    Write your SQL query here
+SELECT name, age
+FROM bears
+WHERE sex = 'F';
 """
 
 select_all_bears_names_and_orders_in_alphabetical_order = """
-    Write your SQL query here
+SELECT name
+FROM bears
+ORDER BY name ASC;
 """
 
 select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
-    Write your SQL query here
+SELECT name, age
+FROM bears
+WHERE alive = 1
+ORDER BY age ASC;
 """
 
 select_oldest_bear_and_returns_name_and_age = """
-    Write your SQL query here
+SELECT name, age
+FROM bears
+WHERE age = (SELECT MAX(age) FROM bears) AND name IS NOT NULL;
 """
+
 select_youngest_bear_and_returns_name_and_age = """
-    Write your SQL query here
+SELECT name, age
+FROM bears
+WHERE age = (SELECT MIN(age) FROM bears);
 """
